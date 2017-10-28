8781e4518877cc58fee5be7520774f96675daa3a
vchrpng <vchrpng@gmail.com>
2017-10-28 13:38:07 +0700

    Create New Containers and Add Routes

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
+import Main from './Main'
+import LoginAdmin from './LoginAdmin'
+import Products from './Products'
+import ManageUsers from './ManageUsers'
+import Orders from './Orders'
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Admin
-        test git
+      <Router>
+      <div>
+        <Route exact path="/" component={LoginAdmin} />
+        <Route path="/main" component={Main} />
+        <Route path="/products" component={Products} />
+        <Route path="/orders" component={Orders} />
+        <Route path="/manage-users" component={ManageUsers} />
       </div>
-    );
+      </Router>
+    )
   }
 }
 
---
+++ src/containers/LoginAdmin.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class LoginAdmin extends Component {
+    render() {
+        return (
+            <div>
+                Login Admin
+            </div>
+        )
+    }
+}
+
+export default LoginAdmin
